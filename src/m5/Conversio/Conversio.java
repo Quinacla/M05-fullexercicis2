969b11ad2497c13472fd783458cdbc1dd0a2c753
quim.navcla.dam_alum <quim.navcla.dam_alum@203-01>
2022-11-03 15:31:39 +0100

    Sense faltes.

---
+++ src/m5/Conversio/Conversio.java
@@ -0,0 +1,23 @@
+package m5.Conversio;
+
+import java.util.Scanner;
+
+public class Conversio {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Introdueix en €: ");
+        int valor_euros = sc.nextInt();
+                
+        double tipus_de_canvi = 1.15d;
+        System.out.println(valor_euros + "€ acatualment equivalen a: " + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+    } 
+}
+
+
+
+
+
+ 
+
+    
